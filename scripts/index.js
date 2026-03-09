3750f9f8001c126f8da1549fa4017881d7124c90
SadmanG <sadmangergees@gmail.com>
2026-03-09 19:31:27 +0600

    Tried to fix Modal

--- scripts/index.js
+++ scripts/index.js
@@ -43,58 +43,31 @@ const displayCard = (cards) =>{
 };
 loadCards();
 
-const displayLevelWord=(words)=>{
-    const wordContainer=document.getElementById("word-container");
-    wordContainer.innerHTML="";
-
-    if(words.length == 0){
-        wordContainer.innerHTML=`
-        <div class="text-center col-span-full rounded-xl py-10 space-y-6 font-bangla">
-        <img class="mx-auto" src="./assets/alert-error.png">
-        <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
-        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান।</h2>
-        </div>
-        `;
-        manageSpinner(false);
-        return;
-    }
-
-    // {
-    //     "id": 82,
-    //     "level": 1,
-    //     "word": "Car",
-    //     "meaning": "গাড়ি",
-    //     "pronunciation": "কার"
-    // }
-
-    words.forEach(word =>{
-        const card=document.createElement("div");
-        card.innerHTML=`
-        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word ? word.word :"শব্দ পাওয়া যায়নি।"}</h2>
-            <p class="font-semibold">Meaning /Pronounciation</p>
-            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning :"অর্থ পাওয়া যায়নি।"} / ${word.pronunciation ? word.pronunciation :"উচ্চারণ পাওয়া যায়নি।"}"</div>
-            <div class="flex justify-between items-center">
-                <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
-            </div>
-        </div>
-        `;
-        wordContainer.append(card);
-    });
-    manageSpinner(false);
+const loadWordDetail = async(id)=>{
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    const res = await fetch(url);
+    const details = await res.json();
+    displayWordDetail(details.data);
 };
 
-document.getElementById("btn-search").addEventListener("click",()=>{
-    const input = document.getElementById("input-search");
-    const searchValue = input.value.trim().toLowerCase();
-    console.log(searchValue);
-
-    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
-    .then(res=>res.json())
-    .then(data=>{
-        const allWords = data.title;
-        const filterWords = allWords.filter((word)=>word.title.toLowerCase().includes(searchValue));
-        displayLevelWord(filterWords);
-    });
-})
+const displayWordDetail=(word)=>{
+    const detailsBox = document.getElementById("details-container");
+    detailsBox.innerHTML=`
+    <div class="">
+        <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})</h2>
+      </div>
+      <div class="">
+        <h2 class="text-2xl font-bold">Meaning</h2>
+        <p>${word.meaning}</p>
+      </div>
+      <div class="">
+        <h2 class="text-2xl font-bold">Example</h2>
+        <p>${word.sentence}</p>
+      </div>
+      <div class="">
+        <h2 class="text-2xl font-bold">Synonyms</h2>
+        <div class="">${createElements(word.synonyms)}</div>
+      </div>
+    `;
+    document.getElementById("word_modal").showModal();
+};
